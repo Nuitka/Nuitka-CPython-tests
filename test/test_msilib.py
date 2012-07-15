f2967bff968375f39c4938c580741c18bb4bffdd
Kay Hayen <kayhayen@gmx.de>
2012-07-15 21:24:18 +0200

    Test Environment: Import "test_support" module from "test" package.

--- test/test_msilib.py
+++ test/test_msilib.py
@@ -1,7 +1,7 @@
 """ Test suite for the code in msilib """
 import unittest
 import os
-from test_support import run_unittest, import_module
+from test.test_support import run_unittest, import_module
 msilib = import_module('msilib')
 
 class Test_make_id(unittest.TestCase):
