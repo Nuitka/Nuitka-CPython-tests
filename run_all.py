7944ba7cec56f3a4de3c46cb6364c00d9f0142dc
Kay Hayen <kay.hayen@gmail.com>
2014-06-21 06:49:48 +0200

    Added test runner to compare Nuitka results with CPython results.

---
+++ run_all.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python3.4
+
+from __future__ import print_function
+
+import os, sys, subprocess
+
+# Go its own directory, to have it easy with path knowledge.
+os.chdir( os.path.dirname( os.path.abspath( __file__ ) ) )
+
+search_mode = len( sys.argv ) > 1 and sys.argv[1] == "search"
+
+start_at = sys.argv[2] if len( sys.argv ) > 2 else None
+
+if start_at:
+    active = False
+else:
+    active = True
+
+if "PYTHON" not in os.environ:
+    os.environ["PYTHON"] = sys.executable
+
+def check_output(*popenargs, **kwargs):
+    from subprocess import Popen, PIPE, CalledProcessError
+
+    if 'stdout' in kwargs:
+        raise ValueError('stdout argument not allowed, it will be overridden.')
+    process = Popen(stdout=PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        raise CalledProcessError(retcode, cmd, output=output)
+    return output
+
+version_output = check_output(
+    [os.environ["PYTHON"], "--version"],
+    stderr = subprocess.STDOUT
+)
+
+python_version = version_output.split()[1]
+
+if sys.version.startswith("3"):
+    python_version = python_version.decode()
+
+os.environ[ "NUITKA_EXTRA_OPTIONS" ] = \
+  os.environ.get("NUITKA_EXTRA_OPTIONS", "") + \
+  " --recurse-none"
+
+print("Using concrete python", python_version)
+
+def checkPath(filename, path):
+    global active
+
+    extra_flags = [
+        "silent",
+        "remove_output",
+        # Import test_support which won't be included and potentially others.
+        "binary_python_path",
+        # Ignore warnings about missing imports
+        "ignore_warnings",
+    ]
+
+    # Avoid memory runaway of CPython2.
+    if path == "doctest_generated/test_itertools.py" and python_version < "3":
+        return
+
+    if path == "test/test_shelve.py":
+        extra_flags.append("ignore_stderr")
+
+    if "doctest_generated" in path:
+        if python_version >= "3.4":
+            extra_flags.append("expect_success")
+
+        if filename == "test_generators.py":
+            extra_flags.append("ignore_stderr")
+
+
+    # TODO: These don't compile in debug mode yet, due to missing optimization
+    if "--debug" in os.environ["NUITKA_EXTRA_OPTIONS"]:
+        if filename in ("test_grammar.py", ):
+            return
+
+    # TODO: Some weird effects of exception chaining are going on there, that
+    # need to be examined, but aren't critical.
+    if filename == "test_ast.py" and python_version >= "3.4":
+        extra_flags.append("ignore_stderr")
+
+    result = subprocess.call(
+        "%s %s %s %s" % (
+            sys.executable,
+            os.path.join("..", "..", "bin", "compare_with_cpython"),
+            path,
+            " ".join(extra_flags)
+        ),
+        shell = True
+    )
+
+    if result != 0 and search_mode:
+        sys.exit(result)
+
+def checkDir(directory):
+    global active
+
+    for filename in sorted(os.listdir(directory)):
+        if not filename.endswith(".py") or not filename.startswith("test_"):
+            continue
+
+        if filename == "test_support.py":
+            continue
+
+        path = os.path.join( directory, filename )
+
+        if not active and start_at in ( filename, path ):
+            active = True
+
+        if active:
+            checkPath(filename, path)
+        else:
+            print("Skipping", path)
+
+checkDir("test")
+checkDir("doctest_generated")
