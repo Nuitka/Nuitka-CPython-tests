2fd99c59c81cd96e879fa0e942e503c2f44c4d4d
Kay Hayen <kay.hayen@gmail.com>
2015-07-02 09:42:52 +0200

    Make tests executable with debug Python too.

--- test/test_deque.py
+++ test/test_deque.py
@@ -990,7 +990,7 @@ def test_main(verbose=None):
             support.run_unittest(*test_classes)
             gc.collect()
             counts[i] = sys.gettotalrefcount()
-        print(counts)
+        print("REFCOUNTS", counts)
 
     # doctests
     from test import test_deque
