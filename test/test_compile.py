d3bdedb38b1d6ad5a41cb2c3c20b7d8a4eaa6dcd
Kay Hayen <kay.hayen@gmail.com>
2015-07-15 20:55:51 +0200

    Revert "Don't use "func.func_code.co_varnames" for local variables, these are not yet included."
    This reverts commit a0076367da4d0cb582ccf17247f855eedfffad5f.

--- test/test_compile.py
+++ test/test_compile.py
@@ -393,12 +393,10 @@ if 1:
                 import __mangled_mod
                 import __package__.module
 
-        # Nuitka: Issue#18: http://bugs.nuitka.net/issue18
-        # No support for local variables in "func.func_code.co_varnames"
-        # self.assertIn("_A__mangled", A.f.__code__.co_varnames)
-        # self.assertIn("__not_mangled__", A.f.__code__.co_varnames)
-        # self.assertIn("_A__mangled_mod", A.f.__code__.co_varnames)
-        # self.assertIn("__package__", A.f.__code__.co_varnames)
+        self.assertIn("_A__mangled", A.f.__code__.co_varnames)
+        self.assertIn("__not_mangled__", A.f.__code__.co_varnames)
+        self.assertIn("_A__mangled_mod", A.f.__code__.co_varnames)
+        self.assertIn("__package__", A.f.__code__.co_varnames)
 
     def test_compile_ast(self):
         fname = __file__
