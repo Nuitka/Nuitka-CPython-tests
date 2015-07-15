2494ec52edb752af251b2d7b8ea41bfff28a9f5e
Kay Hayen <kay.hayen@gmail.com>
2015-07-15 23:33:35 +0200

    Disable test part that uses network.

--- test/test_timeout.py
+++ test/test_timeout.py
@@ -137,7 +137,8 @@ class TCPTimeoutTestCase(TimeoutTestCase):
     def tearDown(self):
         self.sock.close()
 
-    def testConnectTimeout(self):
+    # Nuitka: Do not use the network.
+    def notestConnectTimeout(self):
         # Testing connect timeout is tricky: we need to have IP connectivity
         # to a host that silently drops our packets.  We can't simulate this
         # from Python because it's a function of the underlying TCP/IP stack.
@@ -221,13 +222,15 @@ class TCPTimeoutTestCase(TimeoutTestCase):
         with support.transient_internet(self.addr_remote[0]):
             self._sock_operation(1, 0.001, 'connect', self.addr_remote)
 
-    def testRecvTimeout(self):
+    # Nuitka: Do not use the network.
+    def notestRecvTimeout(self):
         # Test recv() timeout
         with support.transient_internet(self.addr_remote[0]):
             self.sock.connect(self.addr_remote)
             self._sock_operation(1, 1.5, 'recv', 1024)
 
-    def testAcceptTimeout(self):
+    # Nuitka: Do not use the network.
+    def notestAcceptTimeout(self):
         # Test accept() timeout
         support.bind_port(self.sock, self.localhost)
         self.sock.listen(5)
