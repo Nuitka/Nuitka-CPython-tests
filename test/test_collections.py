4ba99e13be6b7739b441207a40eb08d626f7228b
Kay Hayen <kay.hayen@gmail.com>
2015-08-23 07:54:28 +0200

    Python3.3: Disable CPython crashing test

--- test/test_collections.py
+++ test/test_collections.py
@@ -1459,6 +1459,10 @@ class TestOrderedDict(unittest.TestCase):
         self.assertTrue(all(type(pair)==list for pair in od.__reduce__()[1]))
 
     def test_reduce_not_too_fat(self):
+        # Nuitka: Older Python give recursion error that is different length.
+        if sys.version_info < (3,4):
+            return
+
         # do not save instance dictionary if not needed
         pairs = [('c', 1), ('b', 2), ('a', 3), ('d', 4), ('e', 5), ('f', 6)]
         od = OrderedDict(pairs)
