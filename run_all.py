a0bb94198f721e466d4d6f444cb8f5bf787ca5ef
Kay Hayen <kay.hayen@gmail.com>
2015-10-24 12:07:48 +0200

    Test Environment: Use common code for test runner details.
    * Also remove no more necessary white listing.

--- run_all.py
+++ run_all.py
@@ -1,75 +1,38 @@
 #!/usr/bin/env python3.4
 
-from __future__ import print_function
+import os, sys
 
-import os, sys, subprocess
-
-# Go its own directory, to have it easy with path knowledge.
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-search_mode = len( sys.argv ) > 1 and sys.argv[1] == "search"
-
-start_at = sys.argv[2] if len( sys.argv ) > 2 else None
-
-if start_at:
-    active = False
-    start_at = start_at.replace("/", os.path.sep)
-else:
-    active = True
-
-if "PYTHON" not in os.environ:
-    os.environ["PYTHON"] = sys.executable
-
-# Make sure we flush after every print, the "-u" option does more than that
-# and this is easy enough.
-def my_print(*args, **kwargs):
-    print(*args, **kwargs)
-
-    sys.stdout.flush()
-
-reportSkip = my_print
-
-def check_output(*popenargs, **kwargs):
-    from subprocess import Popen, PIPE, CalledProcessError
-
-    if 'stdout' in kwargs:
-        raise ValueError('stdout argument not allowed, it will be overridden.')
-    process = Popen(stdout=PIPE, *popenargs, **kwargs)
-    output, unused_err = process.communicate()
-    retcode = process.poll()
-    if retcode:
-        cmd = kwargs.get("args")
-        if cmd is None:
-            cmd = popenargs[0]
-        raise CalledProcessError(retcode, cmd, output=output)
-    return output
-
-version_output = check_output(
-    [os.environ["PYTHON"], "--version"],
-    stderr = subprocess.STDOUT
+# Find common code relative in file system. Not using packages for test stuff.
+sys.path.insert(
+    0,
+    os.path.normpath(
+        os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            ".."
+        )
+    )
+)
+from test_common import (
+    my_print,
+    setup,
+    reportSkip,
+    compareWithCPython,
+    createSearchMode
 )
 
-python_version = version_output.split()[1]
+python_version = setup(needs_io_encoding = True)
 
-if sys.version.startswith("3"):
-    python_version = python_version.decode()
+search_mode = createSearchMode()
 
-os.environ[ "NUITKA_EXTRA_OPTIONS" ] = \
-  os.environ.get("NUITKA_EXTRA_OPTIONS", "") + \
-  " --recurse-none"
-
-my_print("Using concrete python", python_version)
-
-def checkPath(filename, path):
+def checkPath(dirname, filename):
     global active
 
     extra_flags = [
-        "silent",
         "remove_output",
         # Import test_support which won't be included and potentially others.
         "binary_python_path",
-        # Ignore warnings about missing imports
-        "ignore_warnings",
+        # We mean to compile only that one module
+        "recurse_none",
         # Keep us informed about timing, even if concurrent runs spoil the
         # accuracy.
         "timing",
@@ -79,8 +42,10 @@ def checkPath(filename, path):
     ]
 
     # Avoid memory runaway of CPython2.
-    if path == "doctest_generated/test_itertools.py" and python_version < "3":
-        reportSkip("This triggers memory error with CPython2.x", os.path.dirname(path), filename)
+    if dirname == "doctest_generated" and \
+       filename == "test_itertools.py" and \
+       python_version < "3":
+        reportSkip("This triggers memory error with CPython2.x", dirname, filename)
         return
 
     if python_version < "3":
@@ -91,7 +56,7 @@ def checkPath(filename, path):
                         "test_warnings.py", "test_configparser.py"):
             extra_flags.append("ignore_stderr")
 
-    if "doctest_generated" in path:
+    if dirname == "doctest_generated":
         if python_version >= "3.4":
             extra_flags.append("expect_success")
 
@@ -100,7 +65,8 @@ def checkPath(filename, path):
 
             # On Windows with 32 bit, the MemoryError breaks the test
             if os.name == "nt":
-                my_print("Skipping", path, "not enough memory with 32 bits.")
+                reportSkip("not enough memory with 32 bits on Windows", dirname, filename)
+
                 return
     else:
         # TODO: Deprecation warnings for some unknown reason. Need to find
@@ -108,28 +74,22 @@ def checkPath(filename, path):
         if filename in ("test_shutil.py", "test_ntpath.py"):
             extra_flags.append("ignore_stderr")
 
-    # TODO: These don't compile in debug mode yet, due to missing optimization
-    if "--debug" in os.environ["NUITKA_EXTRA_OPTIONS"]:
-        if filename in ("test_grammar.py",):
-            my_print("Skipping (due to warnings issue)", path)
-            return
-
     # TODO: This deadlocks, likely a threading problem.
     if python_version >= "3.4" and \
        filename == "test_concurrent_futures.py":
-        my_print("Skipping (due to threading issue)", path)
+        my_print("Skipping (due to threading issue)", filename)
         return
 
     # TODO: This fails to compiler, super is not fully solved.
     if python_version >= "3.4" and \
        filename == "test_super.py":
-        my_print("Skipping (due to compilation issue)", path)
+        my_print("Skipping (due to compilation issue)", filename)
         return
 
     # TODO: The output from cgit attempts to access locals not in the frame,
     # probably due to chaining problems from above.
     if filename == "test_cgitb.py":
-        my_print("Skipping (due to traceback issue)", path)
+        my_print("Skipping (due to traceback issue)", filename)
         return
 
     if python_version < "3.4":
@@ -224,38 +184,29 @@ def checkPath(filename, path):
             my_print("Skipped, newer CPython gives deprecation warnings.")
             return
 
-    result = subprocess.call(
-        "%s %s %s %s" % (
-            sys.executable,
-            os.path.join("..", "..", "bin", "compare_with_cpython"),
-            path,
-            " ".join(extra_flags)
-        ),
-        shell = True
+    compareWithCPython(
+        dirname     = dirname,
+        filename    = filename,
+        extra_flags = extra_flags,
+        search_mode = search_mode,
+        needs_2to3  = False
     )
 
-    if result != 0 and search_mode:
-        sys.exit(result)
 
-def checkDir(directory):
-    global active
-
-    for filename in sorted(os.listdir(directory)):
+def checkDir(dirname):
+    for filename in sorted(os.listdir(dirname)):
         if not filename.endswith(".py") or not filename.startswith("test_"):
             continue
 
         if filename == "test_support.py":
             continue
 
-        path = os.path.join( directory, filename )
-
-        if not active and start_at in ( filename, path ):
-            active = True
+        active = search_mode.consider(dirname, filename)
 
         if active:
-            checkPath(filename, path)
+            checkPath(dirname, filename)
         else:
-            my_print("Skipping", path)
+            my_print("Skipping", os.path.join(dirname, filename))
 
 checkDir("test")
 checkDir("doctest_generated")
