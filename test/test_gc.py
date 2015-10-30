d630610e939d436fbba96aeaa8bd11eada2916f7
Kay Hayen <kay.hayen@gmail.com>
2015-10-31 00:50:53 +0100

    Disable test parts that depend on unsupported garbage collector specifics.

--- test/test_gc.py
+++ test/test_gc.py
@@ -221,8 +221,10 @@ class GCTests(unittest.TestCase):
         del d
         self.assertEqual(gc.collect(), 2)
 
+    # Nuitka: We won't meet the specific behaviour there, due to optimized
+    # frame handling.
     @refcount_test
-    def test_frame(self):
+    def notest_frame(self):
         def f():
             frame = sys._getframe()
         gc.collect()
@@ -636,8 +638,10 @@ class GCTests(unittest.TestCase):
         do_work()
         gc.collect() # this blows up (bad C pointer) when it fails
 
+    # Nuitka: Not really a test of Nuitka to execute externally, and we did
+    # disable warnings, so it won't show.
     @cpython_only
-    def test_garbage_at_shutdown(self):
+    def notest_garbage_at_shutdown(self):
         import subprocess
         code = """if 1:
             import gc
@@ -812,7 +816,9 @@ class GCTests(unittest.TestCase):
         del l
         gc.collect()
 
-    def test_get_objects_arguments(self):
+    # Nuitka: We won't meet the specific behaviour there, due to optimized
+    # parameter handling.
+    def notest_get_objects_arguments(self):
         gc.collect()
         self.assertEqual(len(gc.get_objects()),
                          len(gc.get_objects(generation=None)))
