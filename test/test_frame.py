77446cb4a06b1359aa8faac741be6021f09407f0
Kay Hayen <kay.hayen@gmail.com>
2018-07-26 13:51:50 +0200

    Adapted to different repr of compiled frames.

--- test/test_frame.py
+++ test/test_frame.py
@@ -196,13 +196,13 @@ class ReprTest(unittest.TestCase):
         f_this, f_outer, f_inner = frames
         file_repr = re.escape(repr(__file__))
         self.assertRegex(repr(f_this),
-                         r"^<frame at 0x[0-9a-fA-F]+, file %s, line %d, code test_repr>$"
+                         r"^<(compiled_)?frame at 0x[0-9a-fA-F]+, file %s, line %d, code test_repr>$"
                          % (file_repr, offset + 23))
         self.assertRegex(repr(f_outer),
-                         r"^<frame at 0x[0-9a-fA-F]+, file %s, line %d, code outer>$"
+                         r"^<(compiled_)?frame at 0x[0-9a-fA-F]+, file %s, line %d, code outer>$"
                          % (file_repr, offset + 7))
         self.assertRegex(repr(f_inner),
-                         r"^<frame at 0x[0-9a-fA-F]+, file %s, line %d, code inner>$"
+                         r"^<(compiled_)?frame at 0x[0-9a-fA-F]+, file %s, line %d, code inner>$"
                          % (file_repr, offset + 5))
 
 
