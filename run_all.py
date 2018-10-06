9e0c1254d42ffed5767856b8605a26837332ce61
Kay Hayen <kay.hayen@gmail.com>
2018-10-06 11:12:42 +0200

    Enable caching of CPython test results in runner.

--- run_all.py
+++ run_all.py
@@ -22,7 +22,7 @@ from nuitka.tools.testing.Common import (
     createSearchMode
 )
 
-python_version = setup(needs_io_encoding = True)
+python_version = setup(suite = "CPython26", needs_io_encoding = True)
 
 search_mode = createSearchMode()
 
@@ -38,7 +38,9 @@ def checkPath(dirname, filename):
         "recurse_none",
         # Use the original __file__ value, at least one case warns about things
         # with filename included.
-        "original_file"
+        "original_file",
+        # Cache the CPython results for re-use, they will normally not change.
+        "cpython_cache",
     ]
 
     if python_version < b"2.7" and \
