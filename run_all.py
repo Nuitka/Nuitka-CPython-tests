94a4220e2930beb1755f0de50e2a2ced39d90d48
Kay Hayen <kay.hayen@gmail.com>
2019-09-22 13:27:01 +0200

    Test Environment: Make sure suite directory is in PYTHONPATH

--- run_all.py
+++ run_all.py
@@ -21,6 +21,7 @@ sys.path.insert(
 # isort:start
 
 from nuitka.tools.testing.Common import (
+    addToPythonPath,
     compareWithCPython,
     createSearchMode,
     my_print,
@@ -102,6 +103,8 @@ python_version = setup(suite="CPython26", needs_io_encoding=True)
 setupCacheHashSalt(".")
 
 search_mode = createSearchMode()
+
+addToPythonPath(os.path.abspath("."), in_front=True)
 
 checkDir("test")
 checkDir("doctest_generated")
