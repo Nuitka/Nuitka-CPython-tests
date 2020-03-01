9d533a56bc506226da9696e138241dc5ad199b4f
Kay Hayen <kay.hayen@gmail.com>
2020-03-01 10:51:10 +0100

    Remove useless runner output

--- run_all.py
+++ run_all.py
@@ -229,7 +229,6 @@ def checkPath(dirname, filename):
             reportSkip("syntax error with newer Python", filename, dirname)
             return
 
-
     compareWithCPython(
         dirname=dirname,
         filename=filename,
@@ -251,8 +250,6 @@ def checkDir(dirname):
 
         if active:
             checkPath(dirname, filename)
-        else:
-            my_print("Skipping", os.path.join(dirname, filename))
 
 
 python_version = setup(suite="CPython35", needs_io_encoding=True)
