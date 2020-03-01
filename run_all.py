acb33f1cd780bad5ddf51cef438a626186e1998d
Kay Hayen <kay.hayen@gmail.com>
2020-03-01 10:48:14 +0100

    Remove useless runner output

--- run_all.py
+++ run_all.py
@@ -24,7 +24,6 @@ from nuitka.tools.testing.Common import (
     addToPythonPath,
     compareWithCPython,
     createSearchMode,
-    my_print,
     reportSkip,
     setup,
     setupCacheHashSalt,
@@ -95,8 +94,6 @@ def checkDir(dirname):
 
         if active:
             checkPath(dirname, filename)
-        else:
-            my_print("Skipping", os.path.join(dirname, filename))
 
 
 python_version = setup(suite="CPython26", needs_io_encoding=True)
