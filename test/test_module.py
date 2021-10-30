f5fabf607260c25ad3cd8ce185d5ea3833f653ca
Kay Hayen <kay.hayen@gmail.com>
2021-10-30 14:41:37 +0200

    Disable useless test part that checks forking Python

--- test/test_module.py
+++ test/test_module.py
@@ -267,7 +267,8 @@ a = A(destroyed)"""
                          '{!r} does not end with {!r}'.format(r, ends_with))
 
     @requires_type_collecting
-    def test_module_finalization_at_shutdown(self):
+    # Nuitka: Do not fork Python processes, no point.
+    def notest_module_finalization_at_shutdown(self):
         # Module globals and builtins should still be available during shutdown
         rc, out, err = assert_python_ok("-c", "from test import final_a")
         self.assertFalse(err)
