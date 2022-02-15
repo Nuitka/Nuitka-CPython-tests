95dbb87ba4b1943bab75dc51661bb979691260be
Kay Hayen <kay.hayen@gmail.com>
2022-02-15 10:51:03 +0100

    Disable forking of Python processes, not useful

--- test/support/script_helper.py
+++ test/support/script_helper.py
@@ -60,6 +60,9 @@ _PythonRunResult = collections.namedtuple("_PythonRunResult",
 
 # Executing the interpreter in a subprocess
 def run_python_until_end(*args, **env_vars):
+    # Nuitka: These don't do anything for us, so disable it, may cause failures
+    # for CPython.
+    return None
     env_required = interpreter_requires_environment()
     if '__isolated' in env_vars:
         isolated = env_vars.pop('__isolated')
@@ -135,6 +138,9 @@ def assert_python_ok(*args, **env_vars):
     Python is started in isolated mode (command line option -I),
     except if the __isolated keyword is set to False.
     """
+    # Nuitka: These don't do anything for us, so ignore it
+    return 0, b"", b""
+
     return _assert_python(True, *args, **env_vars)
 
 def assert_python_failure(*args, **env_vars):
@@ -145,6 +151,10 @@ def assert_python_failure(*args, **env_vars):
 
     See assert_python_ok() for more options.
     """
+
+    # Nuitka: These don't do anything for us, so ignore it
+    return 1, b"", b""
+
     return _assert_python(False, *args, **env_vars)
 
 def spawn_python(*args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kw):
@@ -153,6 +163,10 @@ def spawn_python(*args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kw):
     kw is extra keyword args to pass to subprocess.Popen. Returns a Popen
     object.
     """
+    # Nuitka: These don't do anything for us, so disable it, may cause failures
+    # for CPython.
+    return None
+
     cmd_line = [sys.executable, '-E']
     cmd_line.extend(args)
     # Under Fedora (?), GNU readline can output junk on stderr when initialized,
