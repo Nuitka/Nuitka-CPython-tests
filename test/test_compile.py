665c4079293a023d3c5581cc4569df61a5bc68a1
Kay Hayen <kay.hayen@gmail.com>
2024-12-02 01:16:12 -0800

    Python3.13: Change formatting to avoid traceback format differences

--- test/test_compile.py
+++ test/test_compile.py
@@ -1354,9 +1354,7 @@ class TestSourcePositions(unittest.TestCase):
     def assertOpcodeSourcePositionIs(self, code, opcode,
             line, end_line, column, end_column, occurrence=1):
 
-        for instr, position in zip(
-            dis.Bytecode(code, show_caches=True), code.co_positions(), strict=True
-        ):
+        for instr, position in zip(dis.Bytecode(code, show_caches=True), code.co_positions(), strict=True):
             if instr.opname == opcode:
                 occurrence -= 1
                 if not occurrence:
@@ -1385,9 +1383,7 @@ class TestSourcePositions(unittest.TestCase):
 
         compiled_code, _ = self.check_positions_against_ast(snippet)
 
-        self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_OP',
-            line=10_000 + 2, end_line=10_000 + 2,
-            column=2, end_column=8, occurrence=1)
+        self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_OP', line=10_000 + 2, end_line=10_000 + 2, column=2, end_column=8, occurrence=1)
         self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_OP',
             line=10_000 + 4, end_line=10_000 + 4,
             column=2, end_column=9, occurrence=2)
@@ -1706,8 +1702,7 @@ class TestSourcePositions(unittest.TestCase):
         compiled_code, _ = self.check_positions_against_ast(snippet)
         self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_SUBSCR',
             line=1, end_line=1, column=13, end_column=21)
-        self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_OP',
-            line=1, end_line=1, column=9, end_column=21, occurrence=1)
+        self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_OP', line=1, end_line=1, column=9, end_column=21, occurrence=1)
         self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_OP',
             line=1, end_line=1, column=9, end_column=26, occurrence=2)
         self.assertOpcodeSourcePositionIs(compiled_code, 'BINARY_OP',
