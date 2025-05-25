526379cb3221832d852817139f2a6470002a749c
Kay Hayen <kay.hayen@gmail.com>
2025-05-25 20:48:16 +0200

    Follow move to convertPython to new module.

--- update_doctest_generated.py
+++ update_doctest_generated.py
@@ -35,8 +35,8 @@ import test.test_unpack_ex
 import test.test_weakref
 
 from nuitka.tools.quality.auto_format.AutoFormat import cleanupWindowsNewlines
-from nuitka.tools.testing.Common import convertToPython, goMainDir
-
+from nuitka.tools.testing.Common import goMainDir
+from nuitka.tools.testing.DocTests import convertToPython
 goMainDir()
 
 if not os.path.exists("doctest_generated"):
