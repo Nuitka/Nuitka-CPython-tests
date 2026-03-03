dfd6f2e598ab1642586a2844967acc8045d65e4f
Peter Bierma <zintensitydev@gmail.com>
2026-03-02 22:50:16 -0500

    Fix test_scope.

--- test/test_scope.py
+++ test/test_scope.py
@@ -605,7 +605,8 @@ class ScopeTests(unittest.TestCase):
 
         self.assertRaises(TypeError, sys.settrace)
 
-    def testEvalExecFreeVars(self):
+    # Nuitka: We don't support dynamically using __code__
+    def notestEvalExecFreeVars(self):
 
         def f(x):
             return lambda: x + 1
