0776be1ad72f3efc2b0ed122766f1b8f42427a09
Kay Hayen <kay.hayen@gmail.com>
2026-05-19 20:14:45 +0200

    Disabled test part that requires frame timing

--- test/test_frame.py
+++ test/test_frame.py
@@ -275,7 +275,8 @@ class TestIncompleteFrameAreInvisible(unittest.TestCase):
         assert_python_ok("-c", code)
 
     @support.cpython_only
-    def test_sneaky_frame_object(self):
+    # Nuitka: We handle frame timing differently, so this test is not applicable.
+    def notest_sneaky_frame_object(self):
 
         def trace(frame, event, arg):
             """
