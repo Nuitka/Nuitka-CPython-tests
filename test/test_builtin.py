d99e902d5a102b6967e79105e3ed0a2d84d909b5
Kay Hayen <kay.hayen@gmail.com>
2026-09-15 14:39:28 +0200

    fixup! Disable test parts not working before 3.13.3

--- test/test_builtin.py
+++ test/test_builtin.py
@@ -1529,7 +1529,10 @@ class BuiltinTest(unittest.TestCase):
             sys.stdout = savestdout
             fp.close()
 
-    @unittest.skipIf(sys.version_info < (3, 13), "Causes segfault on CPython < 3.13")
+    @unittest.skipIf(
+        sys.version_info < (3, 13, 3),
+        "Causes segfault on CPython < 3.13.3 (gh-130163)",
+    )
     def test_input_gh130163(self):
         class X(io.StringIO):
             def __getattribute__(self, name):
