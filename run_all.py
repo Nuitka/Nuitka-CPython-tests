748aa6aab104f19fee156a2c3a5512aa259407c5
Kay Hayen <kay.hayen@gmail.com>
2026-09-25 16:07:36 +0200

    Enable test_patma, finally fully compatible

--- run_all.py
+++ run_all.py
@@ -24,7 +24,6 @@ from nuitka.tools.testing.Common import (
     addToPythonPath,
     compareWithCPython,
     createSearchMode,
-    my_print,
     reportSkip,
     setup,
     setupCacheHashSalt,
@@ -55,11 +54,6 @@ def checkPath(dirname, filename):
     # TODO: This deadlocks, likely a threading problem.
     if python_version >= (3, 4) and filename == "test_concurrent_futures.py":
         reportSkip("Skipping (due to threading issue)", dirname, filename)
-        return
-
-    # TODO: This is a problem, we are not complete with | assignments yet
-    if filename == "test_patma.py":
-        my_print("Skipping (due to INCOMPLETE implementation)", filename)
         return
 
     if filename == "test_htmlparser.py":
